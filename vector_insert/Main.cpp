37c2e77dcf84a79a6f8a1641ac757e6ad71cc1bd
mrmor <mrmor@DESKTOP-FRVGHEE>
2024-05-11 14:02:33 +0300

    Add project files.

---
+++ vector_insert/Main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	std::string name;
+	int number;
+	/*int add = 1;
+	int del = 2;
+	int stop = 3;*/
+
+	std::vector<std::string> string_arr = { "hello", "world","vector" };
+
+	std::cout << std::endl;
+	for (std::string arr_s : string_arr)
+		std::cout << arr_s << std::endl;
+
+	std::cout << " \n";
+	std::cout << "1 - Добавить значение : 2 - Удалить значение : 3 - Завершить программу \n";
+	std::cout << " \n";
+	std::cin >> number;
+
+	switch (number)
+	{
+	case 1:
+		std::cout << "Добавляем значение укажите Позицию и Название : \n";
+		std::cin >> number >> name;
+		string_arr.insert(string_arr.begin() + number, name);
+
+		for (std::string arr_s : string_arr)
+			std::cout << arr_s << std::endl;
+		break;
+	case 2:
+		std::cout << "Какую позицию удалить введите номер : \n";
+		std::cin >> number;
+		string_arr.erase(string_arr.begin() + number);
+
+		for (std::string arr_s : string_arr)
+			std::cout << arr_s << std::endl;
+		break;
+	default:
+		std::cout << "Завершение программы : " << "\n";
+		break;
+	}
+}
